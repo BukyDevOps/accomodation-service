8944d79b411cde563d4b218d74fbca35cf915eec
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2023-07-28 14:20:06 +0200

    feat: security added

---
+++ src/main/java/buky/example/accomodationservice/config/WebConfig.java
@@ -0,0 +1,31 @@
+package buky.example.accomodationservice.config;
+
+import buky.example.accomodationservice.security.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authorizationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authorizationInterceptor);
+    }
+}
+
